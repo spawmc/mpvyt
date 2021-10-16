f09d75d8d979de60e8be52736bed525710e3deb6
spawmc <spawnmcsqrt@gmail.com>
2021-10-15 23:24:18 -0500

    Key calls functionality was added

--- mpvyt.sh
+++ mpvyt.sh
@@ -17,6 +17,34 @@ function ctrl_c () {
     tput cnorm; exit 0
 }
 
+function key_call () {
+    case "${1}" in
+        l)
+            kill -9 "${last_pid}"
+            echo "Next track"
+            ;;
+        n)
+            echo "n press"
+            ;;
+        "?")
+            echo "Choise not found";
+            ;;
+    esac
+}
+
+function playing_whitout_video () {
+    tput civis
+    for link in "${links_playlist[@]}"; do
+        mpv --ytdl "${link}" --ytdl-format=249 > /dev/null 2>&1  &
+        last_pid="${!}";
+        while true; do
+            read -rsn1 input
+            key_call "${input}";
+        done
+    done
+    tput cnorm
+}
+
 function main () {
     dependency-check "jq" "youtube-dl" "curl" "mpv"
 }
